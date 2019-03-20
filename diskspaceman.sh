e62566efbc6f35172b415b782e84e5f8c9434fb2
AK Sarav <sarasoft@live.com>
2019-03-20 17:08:51 -0400

    Usage of Fuser to ignore the current log files

--- diskspaceman.sh
+++ diskspaceman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Author: Sarav - aksarav@mwinventory.in
+# Author: Sarav - aksarav@middlewareinventory.com
 # Purpose: LogRotation&LogPurging
 # Name: diskspaceman.sh
 #
@@ -12,16 +12,32 @@ LOGDATE=`date +%d-%m-%y' '%H':'%M':'%S`
 #Determine the username
 if [ `whoami` == "weblogic" ]
 then
-        DIRTOSEARCH="/apps/weblogic/domains/*/*/*/logs /opt/weblogic/domains/*/*/*/logs /opt/weblogic/logs/* /apps/weblogic/logs/*"
+        DIRTOSEARCH="/apps/weblogic/domains/*/servers/*/logs /opt/weblogic/domains/*/servers/*/logs /opt/weblogic/logs/* /apps/weblogic/logs/*"
         LOGSDIRS=`ls -d $DIRTOSEARCH 2>/dev/null`
 elif [ `whoami` == "tomcat" ]
 then
         DIRTOSEARCH="/opt/tomcat/instances/*/logs /apps/tomcat/instances/*/logs /opt/tomcat/logs/* /apps/tomcat/logs/*"
         LOGSDIRS=`ls -d $DIRTOSEARCH 2>/dev/null`
+elif [ `whoami` == "artifactory" ]
+then
+        DIRTOSEARCH="/opt/jfrog/artifactory/tomcat/logs"
+        LOGSDIRS=`ls -d $DIRTOSEARCH 2>/dev/null`   
+elif [ `whoami` == "anthill" ]
+then
+        DIRTOSEARCH="/opt/artifactory/logs"
+        LOGSDIRS=`ls -d $DIRTOSEARCH 2>/dev/null` 
+elif [ `whoami` == "domain" ]
+then
+        DIRTOSEARCH="/opt/np/domain/tomcat/logs"
+        LOGSDIRS=`ls -d $DIRTOSEARCH 2>/dev/null`
+elif [ `whoami` == "gym" ]
+then
+        DIRTOSEARCH="/opt/np/gym/tomcat/logs"
+        LOGSDIRS=`ls -d $DIRTOSEARCH 2>/dev/null`        
 else
         echo -e "Correct the Errors before proceeding\n"
         echo -e "ERROR: Invalid User to run the script"
-        echo -e "Valid Users are \n\t 1) tomcat \n\t 2) weblogic\n"
+        echo -e "Valid Users are \n\t 1) tomcat \n\t 2) weblogic\n\t 3) artificatory"
         exit 3
 fi
 
@@ -60,7 +76,7 @@ LOGROTATE()
         for OUTFILE in $@
         do
                 sed "s/REPLACELOGFILE/`echo $OUTFILE|sed -f $BASEDIR/front2back.sed`/g" $BASEDIR/logrotate-out.conf-template  > $BASEDIR/logrotate-out.conf
-                logrotate -s /tmp/diskspaceman-lgrt-statusfile -f $BASEDIR/logrotate-out.conf
+                /usr/sbin/logrotate -s /tmp/diskspaceman-lgrt-statusfile -f $BASEDIR/logrotate-out.conf
 
                 if [ $? -eq 0 ]
                 then
@@ -74,15 +90,17 @@ LOGROTATE()
 
 PURGE()
 {
-        FILETOREMOVE=`find . -type f -mtime +$RETENTION`
-        LOG "REMOVING THE $RETENTION DAYS OLD FILES"
+        FILETOREMOVE=`find $DIR -type f -mtime +$RETENTION -name "*.gz$"`
+        LOG "REMOVING THE $RETENTION DAYS OLD FILES WITH GZ EXTENSION"
         LOG "LIST OF FILES GOING TO BE REMOVED: [ `echo $FILETOREMOVE|sed 's/ /,/g' ` ]"
-        find . -type f -mtime +$RETENTION -exec rm -vf {} \;
+        find $DIR -type f -mtime +$RETENTION -name "*.gz$" -exec rm -vf {} \;
         LOG
-                LOG "G-ZIPPING THE OTHER AVAILABLE LOGS"
-                if [ `find . -type f|egrep -v  "*.out$|*.log$|*.gz$"|wc -l` -gt 0 ]
+                LOG "G-ZIPPING THE OTHER AVAILABLE LOGS OLDER THAN 5 DAYS"
+                                # PURGING THE LOG FILES OLDER THAN TWO DAYS
+                if [ `find $DIR -type f -mtime +5|egrep -v  "*.gz$|*.cfg$|*.pid$"|wc -l` -gt 0 ]
                 then
-                        find . -type f|egrep -v  "*.out$|*.log$|*.gz$"|xargs gzip -v
+                        #find . -type f -mtime +5|egrep -v  "*.gz$|*.cfg$|*.pid$"|xargs gzip -v
+                        find . -type f -mtime +5 -not -name "*.gz" -not -name "*.cfg" -not -name "*.pid" -exec  gzip -v --suffix $(date +".%m-%d-%Y.gz") {} \;
                 else
                         LOG "NO LOGS FOUND FOR COMPRESS (GZIP)..SKIPPING"
                 fi
@@ -102,13 +120,13 @@ do
                 # INTO THE DIRECTORY
         LOG "PROCESSING DIRECTORY: $DIR"
         LOG
-        LISTOFFILES=`ls $DIR|egrep -i "*.log$|*.out$"`
-        LISTOFFILESFULL=`ls $DIR/*|egrep -i "*.log$|*.out$"`
+                # Consider only the files modified today
+        LISTOFFILES=`find $DIR -mtime -1 -name "*.log" -o -mtime -1 -name "*.out"`
         cd $DIR
         LOG "LIST OF FILES FOUND FOR LOGROTATION: [ `echo $LISTOFFILES|sed 's/ /,/g' ` ]"
 
         #Initiate Log Rotation for these files
-        LOGROTATE $LISTOFFILESFULL
+        LOGROTATE $LISTOFFILES
 
                 #PURGING PROCESS STARTS
                 LOG
